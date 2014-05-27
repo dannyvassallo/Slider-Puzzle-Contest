e5843b826bb9aee878deac55e791bb785ff7a6df
Dan Vassallo <danielvassallo87@gmail.com>
2014-05-27 15:15:02 -0400

    Slider Puzzle with Reward Modal

---
+++ source/javascripts/_app.js.coffee
@@ -0,0 +1,4 @@
+$ ->
+  window.$share_modal     = $("#share-modal")
+
+  window.share_modal      = $share_modal.remodal()
---
+++ source/javascripts/all.js
@@ -0,0 +1,12 @@
+//= require vendor/_jquery.min.js
+
+//= require _app.js
+
+
+//= require vendor/_jquery.browser
+//= require vendor/_jquery.jqpuzzle.full
+//= require vendor/_jquery.remodal
+
+//= require _myscripts.js
+
+
