f077f2295bb0d37e4964cd3abefbfd5ab6883832
Dan Vassallo <danielvassallo87@gmail.com>
2014-06-25 17:36:20 -0400

    Added Validation and Refresh

--- source/javascripts/_myscripts.js
+++ source/javascripts/_myscripts.js
@@ -1,2 +1,49 @@
+$(function(){
+  // run after form submit
+  var after_submit = function(form){
+    $("#ss-form").css("display","none");
+    $(".remodal-confirm.modal-ok-style").css("display", "visible");
+    form.submit();    
+  }
+
+  // wait for $.valiator to be loaded
+  var t = setInterval(function(){
+    // keep waiting if $.validator hasn't loaded yet
+    if(!$.validator) return;
+    clearInterval(t);
+
+    //    custom validators
+    var notplaceholder  = function(value, element, params){
+      return value != $(element).attr("placeholder");
+    };
+    var valid_zipcode   = function(value, element, params) {
+      return this.optional(element) || /^\d{5}(?:-\d{4})?$/.test(value);
+    };
+
+    jQuery.validator.addMethod("notplaceholder", notplaceholder, "This field is required.");
+    jQuery.validator.addMethod("zipcode", valid_zipcode, "Please provide a valid zipcode.");
+    // .. custom validators ..
+
+    // do the validate
+    $("#ss-form").validate({
+      // customize submit action
+      "submitHandler": after_submit,
+
+      // don't check validate on these actions
+      "onfocusout": false,
+      "onkeyup":    false,
+      "onclick":    false,
+
+      "rules": {
+        // email
+        "entry.1.single": {
+          "notplaceholder": true,
+          "email":          true
+        }
+      }
+    });
+  },1);
 
 
+
+});
